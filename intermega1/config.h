ec8a3fa2b47737e67424811c34206d7b8a8a059c
fishrockz <willsfor@gmail.com>
2013-09-14 15:49:40 +0100

    added a config file

---
+++ intermega1/config.h
@@ -0,0 +1,12 @@
+#define print_input	1
+#define print_output	1
+
+#define print_sonar	0
+#define print_comp	0
+
+#define print_just_one	1
+#define run_just_one	1
+
+
+
+#define print_raw_interupts 0
